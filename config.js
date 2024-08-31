36acf6822fa8d2a62226ff2a34d3e26ac751e639
aritra <78933353+aritra-exe@users.noreply.github.com>
2024-08-31 07:41:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
   commandsDir: './commands',  
   embedColor: "#1db954",
   musicardTheme:"themes16", //Goes from themes1 to themes19
-  activityName: "to Moosetape 24/7", // This is bot status Write Anything here 
+  activityName: "to Moosetape 24/7, to Sidhu Moosewala 24/7", // This is bot status Write Anything here 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/xQF9f9yUEM",
   CheckmarkIcon: "https://cdn.discordapp.com/attachments/1230824451990622299/1230836684774576168/7762-verified-blue.gif",
